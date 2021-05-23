c5ff2434b1635fb4193aff47099efc4deb2851ef
Arvind Shrivastava <73704515+arv011@users.noreply.github.com>
2021-05-23 20:03:57 +0530

    Update settings.py

--- mtask/settings.py
+++ mtask/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j0g=s49w5(l*k4&v+cs3jyiwgy448=zy06na6q#ei4-g1dw8k9'
+SECRET_KEY = 'your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
